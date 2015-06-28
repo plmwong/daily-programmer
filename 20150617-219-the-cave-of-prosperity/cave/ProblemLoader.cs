1757af711b58714cfd8bee040f429c963a83c67b
Phillip Wong <wongphil@gmail.com>
2015-06-28 00:19:20 -0700

    problem description for mangling sentences.

--- 20150617-219-the-cave-of-prosperity/cave/ProblemLoader.cs
+++ 20150617-219-the-cave-of-prosperity/cave/ProblemLoader.cs
@@ -3,23 +3,23 @@ using System.Collections.Generic;
 
 namespace cave
 {
-	public class ProblemLoader
-	{
-		public State Load(string filename) 
-		{
-			using (var streamReader = File.OpenText(filename))
-			{
-				var capacity = decimal.Parse(streamReader.ReadLine());
-				var numberOfNuggets = int.Parse(streamReader.ReadLine());
-				var nuggetsList = new List<decimal>();
-				
-				for(int i = 0; i < numberOfNuggets; i++) 
-				{
-					nuggetsList.Add(decimal.Parse(streamReader.ReadLine()));	
-				}
-				
-				return new State(capacity, nuggetsList.ToArray());
-			}
-		}
-	}	
+    public class ProblemLoader
+    {
+        public State Load(string filename)
+        {
+            using (var streamReader = File.OpenText(filename))
+            {
+                var capacity = decimal.Parse(streamReader.ReadLine());
+                var numberOfNuggets = int.Parse(streamReader.ReadLine());
+                var nuggetsList = new List<decimal>();
+
+                for (int i = 0; i < numberOfNuggets; i++)
+                {
+                    nuggetsList.Add(decimal.Parse(streamReader.ReadLine()));
+                }
+
+                return new State(capacity, nuggetsList.ToArray());
+            }
+        }
+    }
 }
